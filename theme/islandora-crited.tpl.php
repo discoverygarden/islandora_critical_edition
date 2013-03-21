6b3bff45c2af572955d1447e60a7adf770af43a5
Alan Stanley <astanley@upei.ca>
2013-03-21 13:23:53 -0300

    fixed behaviours

--- theme/islandora-crited.tpl.php
+++ theme/islandora-crited.tpl.php
@@ -7,10 +7,11 @@
  */
 ?>
 
-<div class="emic-cwrc-wrapper">
- <input id="full-window-button" type="button" value="<?php print t('Full Window'); ?>" />
+
+<div class="islandora-crited-wrapper">
+  <input id="full-window-button" type="button" value="<?php print t('Full Window'); ?>" />
   <input id="bookview_button" type="button" value="<?php print t('Return to Book View'); ?>" />
-  <div class="cwrc-iframe-wrapper">
-    <iframe id="shared_canvas_iframe" src =" <?php print $viewer_url; ?>" scrolling="0" frameborder="0" style="width: 100%; height: 800px;"><?php print t('Errors: unable to load SharedCanavas'); ?></iframe>
+  <div class="islandora-crited-iframe-wrapper">
+    <iframe id="islandora-crited-iframe" src="<?php print $viewer_url; ?>" scrolling="0" frameborder="0" style="width: 100%; height: 800px;"><?php print t('Errors: unable to load SharedCanavas'); ?></iframe>
   </div>
 </div>
