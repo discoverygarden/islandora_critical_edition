9e85959ab7be83a89a87e48d3f56eb60c183df50
William Panting <willtp87@hotmail.com>
2013-10-02 15:32:51 -0300

    writer as an object attribute

--- js/writer_backend_delegate.js
+++ js/writer_backend_delegate.js
@@ -5,7 +5,7 @@
  * @returns {___anonymous83_84}
  */
 function islandoraBackendDelegate(config) {
-	var w = config.writer;
+	this.writer = config.writer;
 	
 	/**
 	 * @memberOf del
@@ -24,9 +24,9 @@ function islandoraBackendDelegate(config) {
 				success: function(data, status, xhr) {
 					if ($.isPlainObject(data)) data = [data];
 					if (data != null) {
-						callback.call(w, data);
+						callback.call(this.writer, data);
 					} else {
-						callback.call(w, []);
+						callback.call(this.writer, []);
 					}
 				},
 				error: function(xhr, status, error) {
@@ -37,9 +37,9 @@ function islandoraBackendDelegate(config) {
 						}
 						var string = lines.join(',');
 						var data = $.parseJSON('['+string+']');
-						callback.call(w, data);
+						callback.call(this.writer, data);
 					} else {
-						callback.call(w, null);
+						callback.call(this.writer, null);
 					}
 				}
 			});
@@ -52,23 +52,23 @@ function islandoraBackendDelegate(config) {
 				dataType: 'jsonp',
 				success: function(data, status, xhr) {
 					if (data != null && data.result != null) {
-						callback.call(w, data.result);
+						callback.call(this.writer, data.result);
 					} else {
-						callback.call(w, []);
+						callback.call(this.writer, []);
 					}
 				},
 				error: function() {
-					callback.call(w, null);
+					callback.call(this.writer, null);
 				}
 			});
 		}
 	};
 	
 	this.validate = function(callback) {
-		var docText = w.fm.getDocumentContent(false);
-		var schemaUrl = w.schemas[w.schemaId].url;
+		var docText = this.writer.fm.getDocumentContent(false);
+		var schemaUrl = this.writer.schemas[this.writer.schemaId].url;
 		var valid = 'pass';
-		callback.call(w, valid);
+		callback.call(this.writer, valid);
 		//TODO: Implement true validator when/if cwrc makes this service available.
 	};
 	
@@ -84,16 +84,16 @@ function islandoraBackendDelegate(config) {
 			async: false,
 			dataType: 'xml',
 			success: function(doc, status, xhr) {
-				window.location.hash = '#'+PID;
-				callback.call(w, doc);
+				window.location.hash = '#' + PID;
+				callback.call(this.writer, doc);
 			},
 			error: function(xhr, status, error) {
-				w.dialogs.show('message', {
+			  this.writer.dialogs.show('message', {
 					title: 'Error',
 					msg: 'An error ('+status+') occurred and '+PID+' was not loaded.',
 					type: 'error'
 				});
-				w.currentDocId = null;
+			  this.writer.currentDocId = null;
 			}
 		});
 	};
@@ -103,8 +103,8 @@ function islandoraBackendDelegate(config) {
 	 * @param callback Called with one boolean parameter: true for successful save, false otherwise
 	 */
 	this.saveDocument = function(callback) {
-		w.mode == w.XMLRDF;
-		var docText = w.fm.getDocumentContent(true);
+	  this.writer.mode == this.writer.XMLRDF;
+		var docText = this.writer.fm.getDocumentContent(true);
 		$.ajax({
 			url : window.parent.Drupal.settings.basePath + 'islandora/cwrcwriter/save_data/' + PID,
 			type: 'POST',
@@ -114,30 +114,30 @@ function islandoraBackendDelegate(config) {
 				"text": docText
 			},
 			success: function(data, status, xhr) {
-				w.editor.isNotDirty = 1; // force clean state
-				w.dialogs.show('message', {
+			  this.writer.editor.isNotDirty = 1; // force clean state
+			  this.writer.dialogs.show('message', {
 					title: 'Document Saved',
-					msg: w.currentDocId+' was saved successfully.'
+					msg: this.writer.currentDocId+' was saved successfully.'
 				});
-				window.location.hash = '#'+w.currentDocId;
+				window.location.hash = '#'+this.writer.currentDocId;
 				if (callback) {
-					callback.call(w, true);
+					callback.call(this.writer, true);
 				}
 			},
 			error: function() {
-				w.dialogs.show('message', {
+			  this.writer.dialogs.show('message', {
 					title: 'Error',
-					msg: 'An error occurred and '+w.currentDocId+' was not saved.',
+					msg: 'An error occurred and '+this.writer.currentDocId+' was not saved.',
 					type: 'error'
 				});
 				if (callback) {
-					callback.call(w, false);
+					callback.call(this.writer, false);
 				}
 			}
 		});
 	};
 	
 	this.getHelp = function(tagName) {
-		return w.u.getDocumentationForTag(tagName);
+		return this.writer.u.getDocumentationForTag(tagName);
 	};
 }
